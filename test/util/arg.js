39e214e630887d8a97695cf9786cef0669ce6b5b
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-05 18:16:42 -0600

    Add arg.Address tests

--- test/util/arg.js
+++ test/util/arg.js
@@ -3,6 +3,26 @@ import test from 'ava'
 
 // Imports
 import arg from '../../src/util/arg'
+import Address from '../../src/primitives/address'
 
-// Placeholder
-test.todo('arg')
+// Test data
+const testAddress = '0xe148E5AA46401b7bEe89D1F6103776ba508024e0'
+
+/**
+ * Succeed Address.
+ */
+test('succeed Address', t => {
+  const address = new Address(testAddress)
+  t.notThrows(() => {
+    arg.Address(address)
+  })
+})
+
+/**
+ * Fail Address.
+ */
+test('fail Address', t => {
+  t.throws(() => {
+    arg.Address('test')
+  })
+})
