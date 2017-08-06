fe60efa3baddb5ee28e3bef0f76b2b2a7f5058b2
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-05 18:21:38 -0600

    Add arg.Taint tests

--- test/util/arg.js
+++ test/util/arg.js
@@ -5,6 +5,7 @@ import test from 'ava'
 import arg from '../../src/util/arg'
 import Address from '../../src/primitives/address'
 import Block from '../../src/primitives/block'
+import Taint from '../../src/primitives/taint'
 
 // Test data
 const testAddress = '0xe148E5AA46401b7bEe89D1F6103776ba508024e0'
@@ -45,5 +46,25 @@ test('succeed Block', t => {
 test('fail Block', t => {
   t.throws(() => {
     arg.Block('test')
+  })
+})
+
+/**
+ * Succeed Taint.
+ */
+test('succeed Taint', t => {
+  const source = new Address(testAddress)
+  const taintItem = new Taint(source)
+  t.notThrows(() => {
+    arg.Taint(taintItem)
+  })
+})
+
+/**
+ * Fail Taint.
+ */
+test('fail Taint', t => {
+  t.throws(() => {
+    arg.Taint('test')
   })
 })
