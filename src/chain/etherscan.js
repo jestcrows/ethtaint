a300c0e76d8bb42747ace71b0d5e73ac1ca69ace
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-06 05:46:01 -0600

    Add Etherscan agent skeleton

---
+++ src/chain/etherscan.js
@@ -0,0 +1,28 @@
+/**
+ * @file Chain agent interfacing with Etherscan.
+ * @module chain/etherscan
+ */
+
+'use strict'
+
+/**
+ * Private members store.
+ * @private
+ */
+const privs = new WeakMap()
+
+/**
+ * Chain agent interfacing with Etherscan.
+ */
+class ChainAgent {
+  /**
+   * No parameters.
+   */
+  constructor () {
+    const priv = {}
+    privs.set(this, priv)
+  }
+}
+
+// Expose
+module.exports = ChainAgent
