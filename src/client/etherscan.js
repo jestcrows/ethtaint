c09e60f3edd6a2a114f25513d03bb09818368e5b
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-06 22:19:37 -0600

    Add ethtaint version to user agent string

--- src/client/etherscan.js
+++ src/client/etherscan.js
@@ -6,14 +6,21 @@
 'use strict'
 
 // Imports
+const version = require('project-version')
 const { URL } = require('url')
 const config = require('config')
 const requestGlobal = require('request-promise-native')
 
+// Construct user agent string
+let userAgent = 'ethtaint'
+if (version) {
+  userAgent += ' ' + version.toString()
+}
+
 // Configure request
 const request = requestGlobal.defaults({
   headers: {
-    'User-Agent': 'ethtaint'
+    'User-Agent': userAgent
   }
 })
 
