fd50bf8f8514c2b1cd547da39032c4819c78c26a
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-07 05:37:01 -0600

    Add quick and dirty Tracker#traceAddresses
    I'm not happy with the quality of this code
    but the deadline looms. There are many
    efficiencies and interface improvements to be had.

--- src/tracker/tracker.js
+++ src/tracker/tracker.js
@@ -6,8 +6,14 @@
 'use strict'
 
 // Imports
+const EventEmitter = require('events')
 const Cache = require('../cache/cache')
 const ChainAgent = require('../chain/etherscan')
+const Address = require('../primitives/address')
+const Taint = require('../primitives/taint')
+
+// Resources
+var undef
 
 /**
  * Private members store.
@@ -16,14 +22,111 @@ const ChainAgent = require('../chain/etherscan')
 const privs = new WeakMap()
 
 /**
+ * Get any tainted untraced address.
+ * @private
+ * @param {Set<Address>} tainted - Tainted addresses.
+ * @param {Set<Address>} traced - Traced addresses.
+ * @return {Address|null} A tainted untraced address.
+ */
+function getTaintedUntraced (tainted, traced) {
+  var address
+  for (address of tainted) {
+    if (!traced.has(address)) {
+      return address
+    }
+  }
+  return null
+}
+
+/**
+ * Process a transaction.
+ */
+function processTransaction (
+  tracker,
+  taint,
+  source,
+  tx,
+  tainted
+) {
+  // No target
+  if (tx.to === null) {
+    return
+  }
+
+  // Input
+  if (tx.to === source) {
+    return
+  }
+
+  // No value
+  if (tx.amount.wei.equals(0)) {
+    return
+  }
+
+  // Already propagated
+  if (tx.hasTaint(taint)) {
+    return
+  }
+
+  // Record propagated
+  tx.addTaint(taint)
+
+  // Add to tainted list
+  tainted.add(tx.to)
+
+  // Already tainted
+  if (tx.to.hasTaint(taint)) {
+    return
+  }
+
+  // Record tainted
+  tx.to.addTaint(taint)
+
+  // Emit tainted
+  tracker.emit('taint', tx.to, taint)
+}
+
+/**
+ * New address tainted.
+ * @event Tracker#taint
+ * @type {Array}
+ * @prop {module:primitives/address.Address} 0
+ *     Tainted address.
+ * @prop {module:primitives/taint.Taint} 1
+ *     Propagated taint.
+ */
+
+/**
+ * Acquiring page of transactions for tainted address.
+ * @event Tracker#page
+ * @type {Array}
+ * @prop {module:primitives/address.Address} 0
+ *     Tainted address.
+ * @prop {number} 1
+ *     Page number.
+ */
+
+/**
+ * Processed transaction for tainted address.
+ * @event Tracker#processedTransaction
+ * @type {Array}
+ * @prop {module:primitives/address.Address} 0
+ *     Tainted address.
+ * @prop {module:primitives/transaction.Transaction} 1
+ *     Processed transaction.
+ */
+
+/**
  * Ethereum taint tracker.
  * @static
+ * @emits Tracker#taint
  */
-class Tracker {
+class Tracker extends EventEmitter {
   /**
    * No parameters.
    */
   constructor () {
+    super()
     const priv = {}
     privs.set(this, priv)
     priv.cache = {
@@ -32,8 +135,96 @@ class Tracker {
       tx: new Cache()
     }
     priv.chain = new ChainAgent(priv.cache)
+    priv.pageSize = 50
+  }
+
+  /**
+   * Trace addresses tainted by specified source.
+   * Starts asynchronous acquisition of all descendant tainting.
+   * Each newly tainted address fires {@link event:Tracker#taint}.
+   * Runs until extant chain data is exhausted.
+   * Returned promise resolves when finished.
+   * @todo Detect and use extant taint with same source.
+   * @param {string} sourceHex
+   *     Source address of taint. As an address hex.
+   * @return {undefined}
+   */
+  async traceAddresses (sourceHex) {
+    // Validate arguments
+    arg.addressHex(sourceHex)
+
+    // Private members
+    const priv = privs.get(this)
+    const cache = priv.cache
+    const chain = priv.chain
+    const pageSize = priv.pageSize
+
+    // Get address
+    let source = await cache.address.get(sourceHex)
+    if (source === undef) {
+      source = new Address(sourceHex)
+      await cache.address.set(sourceHex, source)
+    }
+
+    // Create taint
+    const taint = new Taint(source)
+    source.addTaint(taint)
+
+    // Working data
+    const tainted = new Set()
+    tainted.add(source)
+    const traced = new Set()
+
+    // Trace
+    for (
+      let address = getTaintedUntraced(tainted, traced);
+      address !== null;
+      traced.add(address),
+      address = getTaintedUntraced(tainted, traced)
+    ) {
+      // Get address transactions
+      let txs, numTxs, tx
+      let page = 1
+      do {
+        // Get next page of transactions
+        this.emit(
+          'page',
+          address,
+          page
+        )
+        txs = await chain
+          .listAccountTransactions(address.hex, {
+            page,
+            pageSize
+          })
+        numTxs = txs.length
+
+        // Process transactions
+        for (var i = 0; i < numTxs; i++) {
+          tx = txs[i]
+          await processTransaction(
+            this,
+            taint,
+            address,
+            tx,
+            tainted
+          )
+          this.emit(
+            'processedTransaction',
+            address,
+            tx
+          )
+        }
+
+        // Increment page number
+        page++
+      } while (numTxs === pageSize)
+    }
   }
 }
 
 // Expose
 module.exports = Tracker
+
+// Circular imports
+const arg = require('../util/arg')
---
+++ testm/tracker/tracker.js
@@ -0,0 +1,31 @@
+// Import AVA
+import test from 'ava'
+
+// Imports
+import Tracker from '../../src/tracker/tracker'
+
+// Test data
+const testAddress = '0xD725f517EfdC47e24de8CdAB5DE4BfF3886E49F1'
+
+/**
+ * Trace addresses.
+ */
+test('trace addresses', async t => {
+  const tracker = new Tracker()
+  const sourceHex = testAddress
+  const tainted = new Set()
+  let numTxsProcessed = 0
+  tracker.on('page', (address, page) => {
+    console.log('Getting ' + address.hex + ' page ' + page)
+  })
+  tracker.on('processedTransaction', (address, tx) => {
+    numTxsProcessed++
+    console.log('Processed ' + numTxsProcessed + ' txs')
+  })
+  tracker.on('taint', (address, taint) => {
+    tainted.add(address)
+    console.log('Tainted ' + address.hex + ' ' + tainted.size)
+  })
+  const prom = tracker.traceAddresses(sourceHex)
+  await t.notThrows(prom)
+})
