33afc7cfee78e3b524f020dcd8e7d9e7d7daaf68
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-06 23:07:49 -0600

    Add version to user agent

--- src/util/useragent.js
+++ src/util/useragent.js
@@ -5,11 +5,19 @@
 
 'use strict'
 
+// Imports
+const version = require('project-version')
+
 /**
  * The ethtaint user agent string.
  * @static
  */
 let userAgent = 'ethtaint'
+
+// Add version
+if (version) {
+  userAgent += '/' + version
+}
 
 // Expose
 module.exports = userAgent
