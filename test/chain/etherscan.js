972f37056ef52014acbe036e35ad6ae8aab1296f
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-07 02:50:00 -0600

    Add cache creation to chain/etherscan test

--- test/chain/etherscan.js
+++ test/chain/etherscan.js
@@ -2,13 +2,19 @@
 import test from 'ava'
 
 // Imports
+import Cache from '../../src/cache/cache'
 import ChainAgent from '../../src/chain/etherscan'
 
 /**
  * Create a new Etherscan chain agent.
  */
 test('create', t => {
+  const cache = {
+    block: new Cache(),
+    address: new Cache(),
+    tx: new Cache()
+  }
   t.notThrows(() => {
-    new ChainAgent()
+    new ChainAgent(cache)
   })
 })
