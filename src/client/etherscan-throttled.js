374762b2fe352aae8b42234193319d30a435f7c3
jestcrows <30739072+jestcrows@users.noreply.github.com>
2017-08-07 01:18:06 -0600

    Add rate limiter to client/etherscan-throttled

--- src/client/etherscan-throttled.js
+++ src/client/etherscan-throttled.js
@@ -7,12 +7,32 @@
 
 // Imports
 const RawClient = require('./etherscan')
+const Bottleneck = require('bottleneck')
 
 /**
  * Private members store.
  * @private
  */
 const privs = new WeakMap()
+
+/**
+ * Modules configuration.
+ * @private
+ */
+const moduleConfig = {
+
+  /**
+   * 5 requests per second.
+   * Unlimited queue size.
+   */
+  bottleneck: {
+    maxConcurrent: 5,
+    minTime: 1000,
+    highWater: -1,
+    strategy: Bottleneck.strategy.LEAK,
+    rejectOnDrop: true
+  }
+}
 
 /**
  * Throttled interface to Etherscan API client.
@@ -26,6 +46,13 @@ class Client {
     const priv = {}
     privs.set(this, priv)
     priv.rawClient = new RawClient()
+    priv.limiter = new Bottleneck(
+      moduleConfig.bottleneck.maxConcurrent,
+      moduleConfig.bottleneck.minTime,
+      moduleConfig.bottleneck.highWater,
+      moduleConfig.bottleneck.strategy,
+      moduleConfig.bottleneck.rejectOnDrop
+    )
   }
 }
 
